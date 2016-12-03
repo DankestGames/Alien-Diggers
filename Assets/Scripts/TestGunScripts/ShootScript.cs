00ebc177f919798f654ae511788049ebf32c3474
Dmitri Mikhailov <muxamuxamuxa@gmail.com>
2016-12-03 22:06:14 +0300

    cannon shooting implemented

---
+++ Assets/Scripts/TestGunScripts/ShootScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootScript : MonoBehaviour {
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float firerate = 1f;
+    private float nextFire = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + firerate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+	}
+}
