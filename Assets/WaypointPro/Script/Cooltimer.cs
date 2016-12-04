52240bf8573f8f1c2c20f9b2edc70c60f9f77783
Dmitri Mikhailov <muxamuxamuxa@gmail.com>
2016-12-04 20:32:59 +0300

    Tower rotation and waypoints

---
+++ Assets/WaypointPro/Script/Cooltimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mr1
+{
+    public class Cooltimer : MonoBehaviour
+    {
+        public float cooltime;
+        public Action onFinished;
+
+        public static Cooltimer Set(Component component, float cooltime, Action onFinished)
+        {
+            var cooltimer = component.GetComponent<Cooltimer>();
+            if (cooltimer == null) cooltimer = component.gameObject.AddComponent<Cooltimer>();
+            cooltimer.cooltime = cooltime;
+            cooltimer.onFinished = onFinished;
+            return cooltimer;
+        }
+
+        void Update()
+        {
+            cooltime = Mathf.Max(0f, cooltime - Time.smoothDeltaTime);
+            if (cooltime <= 0f)
+            {
+                if (onFinished != null) onFinished();
+                GameObject.Destroy(this);
+            }
+        }
+    }
+}
---
+++ Assets/WaypointPro/Script/PathData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Mr1
+{
+    public enum PathLineType
+    {
+        Straight,
+        CatmullRomCurve,
+        BezierCurve,
+    }
+
+    public class PathData : ScriptableObject
+    {
+        public bool bShowPath;
+
+        [SerializeField] public string pathName;
+        [SerializeField] public PathLineType lineType;
+        [SerializeField] public Color lineColor = Color.yellow;
+        [SerializeField] public float pointSize = 0.5f;
+
+        [SerializeField] public List<Vector3> points;
+        [SerializeField] public List<Vector3> firstHandles; 
+        [SerializeField] public List<Vector3> secondHandles;
+        [SerializeField] public List<Vector3> linePoints;
+
+        public Vector3 startPoint { get { return linePoints[0]; } }
+        public Vector3 endPoint { get { return linePoints[linePoints.Count-1]; } }
+
+        public PathData()
+        {
+            points = new List<Vector3>();
+            firstHandles = new List<Vector3>();
+            secondHandles = new List<Vector3>();
+            linePoints = new List<Vector3>();
+        }
+
+        
+    }
+}
