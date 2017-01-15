06c6be7de44ab4460496f141cb3bd36a61b787fa
AlexeyKor <alexey_kor@hotmail.com>
2017-01-14 23:22:46 -0800

    Added Menu
    Added main menu scene. Cleaned the repo. Updated gitignore.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("Main Scene", LoadSceneMode.Single);
+    }
+}
