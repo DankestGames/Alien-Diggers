095b1bbda9f2010fbd44913dfd16f9d82c577481
AlexeyKor <alexey_kor@hotmail.com>
2017-01-14 18:55:19 -0800

    Cannon Update
    The cannon now moves smoothly. Instead of moving the whole tower only
    the turret now is moving. Firing behaviour was simplified. Now the
    bullet is just going straight forward.

--- Assets/Scripts/RotateTowardsCursos.cs
+++ Assets/Scripts/RotateTowardsCursos.cs
@@ -3,18 +3,19 @@ using System.Collections;
 
 public class RotateTowardsCursos : MonoBehaviour {
 
-    //public float rotationSpeed = 60f;
+    public float rotationSpeed = 60f;
 
-        // Use this for initialization
+    // Use this for initialization
     void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 dir = Input.mousePosition - pos;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Quaternion targetRotation = Quaternion.LookRotation(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position, Vector3.forward);
+        targetRotation.x = 0;
+        targetRotation.y = 0;
+        // Smoothly rotate towards the target point.
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
