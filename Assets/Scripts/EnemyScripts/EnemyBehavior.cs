8e84ed1d4ca1b32796997f2e8b0104a0e66cbd93
AlexeyKor <alexey_kor@hotmail.com>
2017-01-15 22:18:37 -0800

    Added Enemy Spawns
    Added a second path. Now we create all enemies directly from the script.
    We create these enemies using SpawnManager object at specified "spawns".
    For "spawns" we use startPoints of existing paths on the scene.

--- Assets/Scripts/EnemyScripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyScripts/EnemyBehavior.cs
@@ -5,20 +5,19 @@ public class EnemyBehavior : MonoBehaviour {
 
     public float speed = 3;
     public Transform tower;
-    private string projectileTag = "Projectile";
     public float startHealth = 100;
+    public string pathName = "EnemyWay";
     private float currentHealth;
 	
     // Use this for initialization
 	void Start () {
         currentHealth = startHealth;
-        transform.FollowPath("EnemyWay", speed, Mr1.FollowType.Once);
+        transform.FollowPath(pathName, speed, Mr1.FollowType.Once);
     }
 	
 	// Update is called once per frame
 	void Update () {
         transform.right = tower.position - transform.position;
-        GetComponent<Rigidbody2D>().AddForce(transform.right * 0);
     }
 
     public void TakeDamage(DamageTypeEnum damageType, float amount)
---
+++ Assets/Scripts/SpawnBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBehavior : MonoBehaviour
+{
+
+    private static Object enemyPrefab;
+    private int amount = 6;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyPrefab = Resources.Load("Prefabs/Enemy");
+        Invoke("SpawnEnemy", 2);
+    }
+
+    void SpawnEnemy()
+    {
+        if (amount > 0)
+        {
+            string pathName = amount % 2 == 0 ? "EnemyWay" : "EnemyWay2";
+            // This will spawn enemies at two different spawns - start points of two paths.
+            GameObject newEnemy = Instantiate(enemyPrefab, Mr1.WaypointManager.instance.GetPathData(pathName).startPoint, Quaternion.identity) as GameObject;
+            // Now we need to assign to Enemies correct paths to follow.
+            EnemyBehavior enemyScript = newEnemy.GetComponent<EnemyBehavior>();
+            enemyScript.pathName = pathName;
+            // We also should define the tower object, so enemies will know where to look.
+            enemyScript.tower = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        amount--;
+        Invoke("SpawnEnemy", 2);
+    }
+}
